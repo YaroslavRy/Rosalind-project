857a1a48c465404aac4899c3a3beb5f263ba46b8
YaroslavRy <ya.ryndyk@gmail.com>
2017-11-21 01:09:42 +0200

    Solution of the Mendel's First Law problem
    Introduction to Mendelian Inheritance

--- sol_06_iprb.py
+++ sol_06_iprb.py
@@ -15,7 +15,6 @@ def calc_prob(population):
     total = homo_dominant + hetero + homo_rec
 
     # calc probability of not-dominant groups mating
-
     prob_het_het = (hetero / total) * (hetero - 1) / (total - 1)
     prob_homorec_homorec = (homo_rec / total) * (homo_rec - 1) / (total - 1)
     prob_het_homorec = (hetero / total) * (homo_rec / (total - 1)) + (homo_rec / total) * (hetero / (total - 1))
@@ -23,7 +22,6 @@ def calc_prob(population):
     # 0.25 and 0.5 are probabilities of offspring will be recessive. 
     # Сan be calculated using Punnett square 
     # https://en.wikipedia.org/wiki/Punnett_square
-    
     prob_dominant = 1 - (prob_homorec_homorec + prob_het_het * 0.25 + prob_het_homorec * 0.5)
     
     print(prob_dominant)
---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+
+open(os.path.join(os.path.dirname(__file__), 'data/rosalind_iprb.txt'))
+
+
