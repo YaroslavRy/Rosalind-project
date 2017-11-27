b9193e146b47a72cdc95a7b0641b68581b1bbf61
YaroslavRy <ya.ryndyk@gmail.com>
2017-11-27 23:32:26 +0200

    Finding a Motif in DNA

--- sol_08_subs.py
+++ sol_08_subs.py
@@ -1,23 +1,23 @@
-'''
-Given two strings ss and tt, tt is a substring of ss if tt is contained as a contiguous collection of symbols in ss (as a result, tt must be no longer than ss).
 
-The position of a symbol in a string is the total number of symbols found to its left, including itself (e.g., the positions of all occurrences of 'U' in "AUGCUUCAGAAAGGUCUUACG" are 2, 5, 6, 15, 17, and 18). The symbol at position ii of ss is denoted by s[i]s[i].
+# Given two strings ss and tt, tt is a substring of ss if tt is contained as a contiguous collection of symbols in ss (as a result, tt must be no longer than ss).
 
-A substring of ss can be represented as s[j:k]s[j:k], where jj and kk represent the starting and ending positions of the substring in ss; for example, if ss = "AUGCUUCAGAAAGGUCUUACG", then s[2:5]s[2:5] = "UGCU".
+# The position of a symbol in a string is the total number of symbols found to its left, including itself (e.g., the positions of all occurrences of 'U' in "AUGCUUCAGAAAGGUCUUACG" are 2, 5, 6, 15, 17, and 18). The symbol at position ii of ss is denoted by s[i]s[i].
 
-The location of a substring s[j:k]s[j:k] is its beginning position jj; note that tt will have multiple locations in ss if it occurs more than once as a substring of ss (see the Sample below).
+# A substring of ss can be represented as s[j:k]s[j:k], where jj and kk represent the starting and ending positions of the substring in ss; for example, if ss = "AUGCUUCAGAAAGGUCUUACG", then s[2:5]s[2:5] = "UGCU".
 
-Given: Two DNA strings ss and tt (each of length at most 1 kbp).
+# The location of a substring s[j:k]s[j:k] is its beginning position jj; note that tt will have multiple locations in ss if it occurs more than once as a substring of ss (see the Sample below).
 
-Return: All locations of tt as a substring of ss.
+# Given: Two DNA strings ss and tt (each of length at most 1 kbp).
 
-Sample Dataset
-GATATATGCATATACTT
-ATAT
+# Return: All locations of tt as a substring of ss.
 
-Sample Output
-2 4 10
-'''
+# Sample Dataset
+# GATATATGCATATACTT
+# ATAT
+
+# Sample Output
+# 2 4 10
+
 
 import os
 
