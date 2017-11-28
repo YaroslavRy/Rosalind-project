b114af3ca54c2c458e5ab52984ae4e8f8eea0fdd
YaroslavRy <ya.ryndyk@gmail.com>
2017-11-29 01:53:40 +0200

    Revert "consensus"
    This reverts commit eeedcc4887b380302057bd4485dacbc086fc3a0c.

--- sol_09_cons.py
+++ sol_09_cons.py
@@ -35,20 +35,18 @@ ATGGCACT'''
 
 
 def get_consenus(data):
-	result = []
 	inst = []
 	for i in data:
 		inst.append(Seq(data[i]))
 	m = motifs.create(inst)
-	print(m.consensus)
 	count_matrix = m.counts
 	for key, value in count_matrix.items():
-		print(''.join(key + ': ' + ' '.join(map(str, value))))
+		return "".join(key + ': ' + ' '.join(map(str, value)))
 
 
 if __name__ == '__main__':
 	path_to_file = os.path.join(os.path.dirname(__file__), 'data/rosalind_cons.txt')
 	data = read_data(path=path_to_file)
-	get_consenus(data)
-	# print(result)
+	result = get_consenus(data)
+	print(result)
 
